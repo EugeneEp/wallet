5c01ce85d285a316ddd2e28b3f95811b3b9be8d1
Eugene Epishkin <evgenyepishkin@gmail.com>
2020-11-08 23:06:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ class Configuration(object):
 	DEBUG = True
 	ENV = 'development'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:fzf2e-qg94r-w562r@localhost/acckasdq_1'
+	SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:********@localhost/acckasdq_1'
 	SECRET_KEY = 'L&qqFM,}YU"]9/5#'
 	UPLOAD_FOLDER = 'static/upload/'
